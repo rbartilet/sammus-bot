3a6d30e154fb0cee0bd39e958914b109e7a50204
Rochelle Bartilet <71032863+rbartilet@users.noreply.github.com>
2022-01-17 14:29:11 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,7 @@ import os
 #import dnspython
 import server
 from discord.ext import commands
-from datetime import datetime
+from datetime import datetime, timedelta
 
 bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -24,7 +24,15 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-
+    
+    for i in range(2,30):
+        if 'sammus' in message.content and str(i) in message.content and 'day' in message.content:
+            day_of_year = datetime.now() - timedelta(i)
+            day_of_year = yesterday_of_year.timetuple().tm_yday
+            str_day = ordinal(yesterday_of_year)
+            await message.channel.send('Were you asking for the day ' + str(i) + ' days from now? It will be the ' + str_day + ' day of the year.')
+            break
+    
     if message.author == bot.user:
         return
 
@@ -39,11 +47,23 @@ async def on_message(message):
         str_day = ordinal(day_of_year)
         await message.channel.send('Good morning, happy ' + str_day + ' day of the year.')
     
-    elif 'sammus' in message.content and 'day' in message.content:
+    elif 'sammus' in message.content and 'today' in message.content:
         day_of_year = datetime.now().timetuple().tm_yday
         str_day = ordinal(day_of_year)
-        await message.channel.send('Were you asking for the day? It is the ' + str_day + ' day of the year.')
+        await message.channel.send('Were you asking for the day today? It is the ' + str_day + ' day of the year.')
 
+    elif 'sammus' in message.content and 'yesterday' in message.content:
+        yesterday_of_year = datetime.now() - timedelta(1)
+        yesterday_of_year = yesterday_of_year.timetuple().tm_yday
+        str_day = ordinal(yesterday_of_year)
+        await message.channel.send('Were you asking for the day yesterday? It was the ' + str_day + ' day of the year.')
+        
+    elif 'sammus' in message.content and 'tomorrow' in message.content:
+        tomorrow_of_year = datetime.now() + timedelta(1)
+        tomorrow_of_year = yesterday_of_year.timetuple().tm_yday
+        str_day = ordinal(tomorrow_of_year)
+        await message.channel.send('Were you asking for the day yesterday? It was the ' + str_day + ' day of the year.')
+        
     elif '🅿️' in message.content:
       await message.channel.send('🅿️ is defined as parking.')
         
