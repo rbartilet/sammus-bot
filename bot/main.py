9bc73669353833077d2e77b35827e5a4a4d70c68
Rochelle Bartilet <71032863+rbartilet@users.noreply.github.com>
2022-02-01 15:45:29 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -18,16 +18,15 @@ def ordinal(num):
         suffix = list_of_suffix.get(num % 10, 'th')
     return str(num) + suffix
 
-target_channel = 461601814673096713
-
 @tasks.loop(seconds=20.0)
 async def on_first_of_month():
-    message_channel = bot.get_channel(target_channel)
+    message_channel = bot.get_channel(461601814673096713)
     await message_channel.send("Wake up, it’s the first of the month")
     
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
+    on_first_of_month.start()
 
 @bot.event
 async def on_message(message):
