841c7c6267e83cec495e8bb3bd3cf4ca4fb7adac
Rochelle Bartilet <71032863+rbartilet@users.noreply.github.com>
2022-02-01 15:17:37 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -86,6 +86,10 @@ async def on_message(message):
 async def ping(ctx):
     await ctx.send("pong")
 
+@tasks.loop(hours=24)
+async def on_first_of_month():
+    message_channel = 461601814673096713
+    await message_channel.send("Wake up, it’s the first of the month")
 
 
 server.server()
