f9432fb519b5f1827af955734db782c4b6f0cb98
Rochelle Bartilet <71032863+rbartilet@users.noreply.github.com>
2022-02-05 13:10:17 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -17,7 +17,23 @@ def ordinal(num):
     else:
         suffix = list_of_suffix.get(num % 10, 'th')
     return str(num) + suffix
-    
+
+lot_dict = {1 : "Oscar",
+            2 : "Gabbie",
+            3 : "CJ",
+            4 : "Babs",
+            5 : "Soods",
+            6 : "Russ",
+            7 : "Wilma",
+            8 : "Pat",
+            9 : "Auste",
+            10: "Joseph",
+            11: "Charles",
+            12: "Phil"}
+
+lot_num = 12
+temp_num = 1
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
@@ -69,7 +85,24 @@ async def on_message(message):
         
     elif '🅿️' in message.content and 'sammus' in message.content:
         await message.channel.send('I am not sure what the definition of 🅿️ is.')
+    
+    elif 'sammus' in message.content and 'lottery' in message.content and 'this week' in message.content:
+        if lot_num == 1:
+            await message.channel.send('Ew, it is ' + lot_dict.get(lot_num) + ' this week 🤮.')
+        else:
+            await message.channel.send('Hello, it is ' + lot_dict.get(lot_num) + ' this week.')
         
+    elif 'sammus' in message.content and 'lottery' in message.content and 'next week' in message.content:
+        if lot_num == 12:
+            temp_num = 1
+        else:
+            temp_num = lot_num + 1
+        
+        if temp_num == 1:
+            await message.channel.send('Ew, it is ' + lot_dict.get(temp_num) + ' next week 🤮.')
+        else:
+            await message.channel.send('Hello, it is ' + lot_dict.get(temp_num) + ' next week.')
+       
     elif message.author.id == 284562536378925058 and 'sammus' in message.content: 
         await message.channel.send('Sorry Gabbie, I am a disappointment to you. I do not know how to respond to that.')
     
@@ -83,13 +116,18 @@ async def on_message(message):
         await message.channel.send('Sorry, I do not know how to respond to that.')
 
 @tasks.loop(hours=24)
-async def send():
+async def to_do():
+    if datetime.today().weekday() == 0:
+        lot_num = lot_num + 1
+        if lot_num > 12:
+            lot_num = 1
+    
     if datetime.today().day == 1:
         await bot.get_channel(461601814673096713).send("Wake up, it's the first of the month.")
  
-@send.before_loop
+@to_do.before_loop
 async def before():
     await bot.wait_until_ready()    
 
-send.start()
+to_do.start()
 bot.run(TOKEN)
